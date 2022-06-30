a991024726f512aad463178e6728b3b865b04eb1
junaidNadeem <Junaid.n@upstack.co>
2022-06-30 14:09:13 +0500

    Updates

---
+++ _next/static/NjgZOe8bjxALzJQEiQshu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-39a77f46937f60b1e8bb.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-92e968a2d53055e64b70.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
